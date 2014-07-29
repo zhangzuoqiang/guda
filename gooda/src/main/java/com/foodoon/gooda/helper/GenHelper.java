449b35660d3a544bf024b220cc11fd4298267120
foodoon <foodoon@qq.com>
2014-07-30 06:57:43 +0800

    add

---
+++ gooda/src/main/java/com/foodoon/gooda/helper/GenHelper.java
@@ -0,0 +1,22 @@
+package com.foodoon.gooda.helper;
+
+import com.foodoon.tool.common.StringTool;
+
+/**
+ * Created by foodoon on 2014/7/29.
+ */
+public class GenHelper {
+
+    public static String getDomainName(String tableName,String appName){
+        if(tableName == null || appName == null){
+            return null;
+        }
+        tableName = tableName.toLowerCase();
+        appName = appName.toLowerCase();
+        if(tableName.startsWith(appName+"_")){
+            tableName = tableName.substring(appName.length()+1);
+        }
+        return StringTool.uppercaseFirstLetter(StringTool.underLineStringToCamel(tableName))+"DO";
+    }
+
+}
