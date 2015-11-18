ed8dde43fa57cc322225db3b8723a86a32b5d824
zhangzuoqiang <zhangzuoqiang@live.cn>
2015-11-18 11:26:29 +0800

    update

--- archetype/target/test-classes/projects/basic/project/basic/assembly/sql/create-db.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS `demo`;
-CREATE DATABASE `demo`;
-use `demo`;
-
-
-delete from mysql.user where User = 'demo_user';
-grant select,update,delete,insert on `demo`.* to 'demo_user'@'%' identified by 'demo_pwd';
-grant select,update,delete,insert on `demo`.* to 'demo_user'@'localhost' identified by 'demo_pwd';
-flush privileges;
